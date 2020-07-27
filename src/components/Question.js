8eaa06650afdbd40190c4e71b1d9d9f02932ca9b
Nikos Avramidis <49476564+urbanL1fe@users.noreply.github.com>
2020-07-27 13:47:29 +0300

    made flag smaller

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function Question({ country }) {
   return (
     <>
+      <img width="40" src={country?.flag} />
       <p> What is the capital of {country?.name}?</p>
-      <img src={country?.flag} />
     </>
   );
 }
