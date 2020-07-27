6724ce5a1221cd95fb128ea2c34579f319e38704
Nikos Avramidis <49476564+urbanL1fe@users.noreply.github.com>
2020-07-27 11:58:31 +0300

    updated api to v2, added flags, added memoization to fetch

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Question({ country }) {
-  return <p> What is the capital of {country?.name}?</p>;
+  return (
+    <>
+      <p> What is the capital of {country?.name}?</p>
+      <img src={country?.flag} />
+    </>
+  );
 }
--- src/useGetCountries.js
+++ src/useGetCountries.js
@@ -1,23 +1,27 @@
-import React, { useEffect, useReducer } from "react";
+import React, { useEffect, useRef } from "react";
 import { fetchStatus } from "./utils";
 
 function useGetCountries(dispatch) {
+  let countriesArray = useRef(null);
   useEffect(() => {
     async function getCountries() {
       try {
         dispatch({ type: "FETCH_STARTED" });
-        const res = await fetch("https://restcountries-v1.p.rapidapi.com/all", {
+        /* const res = await fetch("https://restcountries-v1.p.rapidapi.com/all", {
           headers: {
             "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
             "x-rapidapi-key":
               "f72561f52bmsh3b3732be6bd8c0bp171318jsn7831e4e26be9",
             useQueryString: true
           }
-        });
-        const countriesArray = await res.json();
+        }); */
+        if (countriesArray.current === null) {
+          const res = await fetch("https://restcountries.eu/rest/v2/all");
+          countriesArray.current = await res.json();
+        }
         dispatch({
           type: "FETCH_SUCCEDED",
-          countriesArray
+          countriesArray: countriesArray.current
         });
       } catch (error) {
         dispatch({ type: "FETCH_FAILED" });
